8d58e2c315a2a332a07043bae790d1a0cc0d3982
winkey83 <winkey83@gmail.com>
2022-11-27 20:32:37 +0900

    login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onChange = (event) => {
     const {
@@ -27,13 +28,15 @@ const Auth = () => {
       if (newAccount) {
         data = await createUserWithEmailAndPassword(authService, email, password);
       } else {
-        data = await signInWithEmailAndPassword(email, password);
+        data = await signInWithEmailAndPassword(authService, email, password);
       }
       console.log(data);
     } catch(error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+
+  const toggleAccount = () => setNewAccount((prev)=>!prev);
 
   return (
     <div>
@@ -41,7 +44,11 @@ const Auth = () => {
         <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
         <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
         <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount?"Sign In": "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
