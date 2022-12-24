0f937b9aa78d820a8102448dbc305255fb0521c8
winkey83 <winkey83@gmail.com>
2022-12-24 23:02:19 +0900

    add userid

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,40 @@
 import { dbService } from "fbase";
-import { collection, addDoc } from "firebase/firestore/lite";
-import React, { useState } from "react";
-const Home = () => {
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
 
-    const onSubmit = (event) => {
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        dbNweets.forEach((document)=> {
+            const nweetObject = {...document.data(), id: document.id };
+            setNweets((prev)=>[nweetObject, ...prev])
+        });
+    };
+
+    const onSubmit = async (event) => {
         event.preventDefault();
-        const col = collection(dbService, "nweets");
-        // addDoc(col, {
-        //     nweet,
-        //     createdAt: Date.now(),
-        // });
-      
+        await addDoc(collection(dbService, "nweets"), {
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+        });
+    
         setNweet("");
     };
 
     const onChange = (event) => {
+        event.preventDefault();
         const {
             target:{value}
         } = event;
         setNweet(value);
     };
+
+    useEffect( ()=>{
+        getNweets();
+    }, []);
 
     return (
         <div>
@@ -28,6 +42,15 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text"  placeholder="say something." maxLength={120} />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {
+                    nweets.map((nweet)=>(
+                        <div key={nweet.id}>
+                            <h4>{nweet.text}</h4>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
