e826d65f3de4e2a88b41d10adfbebec66edb6977
winkey83 <winkey83@gmail.com>
2022-12-24 11:10:34 +0900

    fire store

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,34 @@
-import React from "react";
-const Home = () => <span>Home</span>;
+import { dbService } from "fbase";
+import { collection, addDoc } from "firebase/firestore/lite";
+import React, { useState } from "react";
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const col = collection(dbService, "nweets");
+        // addDoc(col, {
+        //     nweet,
+        //     createdAt: Date.now(),
+        // });
+      
+        setNweet("");
+    };
+
+    const onChange = (event) => {
+        const {
+            target:{value}
+        } = event;
+        setNweet(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text"  placeholder="say something." maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
 export default Home;
